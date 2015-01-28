b044977570d06ae8b6a97ee06e76ad442d639d6b
James Unger <junger@muteswan.org>
2015-01-28 09:06:06 -0500

    Allow player login and character editing.

--- src/dmhelper.go
+++ src/dmhelper.go
@@ -22,6 +22,8 @@ var (
 	addr          = flag.String("addr", ":8080", "http service address")
 	assets        = flag.String("assets", defaultAssetPath(), "path to assets")
 	homeTempl     *template.Template
+	editPlayerTempl     *template.Template
+	playerIdTempl     *template.Template
 	places        []Place
 	chars         []Char
 	npcs          []Char
@@ -40,6 +42,9 @@ var (
 	objects       []Object
 	scenes        []Scene
 	scene         string
+	players	      []string
+	loggedin      []string
+	charlist	string
 )
 
 type MainData struct {
@@ -80,6 +85,7 @@ type Placement struct {
 }
 
 type Char struct {
+	// Required fields for char or npcs
 	Name       string
 	Class      string
 	Race       string
@@ -95,6 +101,37 @@ type Char struct {
 	Key        string
 	Attacks    []Attack
 	Inventory  []string
+
+
+	// extra fields for player character
+	Inspiration int
+	Speed int
+	Skills string
+	ProfBonus int
+	PassPerception int
+	MiscProfLanguages string
+	HitDice string
+	PersonalityTraits string
+	Background string
+	Ideals string
+	Bonds string
+	Flaws string
+	FeaturesTraits string
+	Treasure string
+	SpellL0 string
+	SpellL1 string
+	SpellL2 string
+	SpellL3 string
+	SpellL4 string
+	SpellL5 string
+	SpellL6 string
+	SpellL7 string
+	SpellL8 string
+	SpellL9 string
+	AttacksJson string
+	AbilitiesJson string
+	InventoryJson string
+	Playername string
 }
 
 type Attack struct {
@@ -113,6 +150,17 @@ type Abilities struct {
 	Int int
 	Wis int
 	Cha int
+}
+
+func (char *Char) JsonIfy() {
+	jsonatt,_ := json.Marshal(char.Attacks)
+	jsoninv,_ := json.Marshal(char.Inventory)
+	jsonabi,_ := json.Marshal(char.Abilities)
+	char.AttacksJson = template.HTMLEscapeString(string(jsonatt))
+	char.InventoryJson = template.HTMLEscapeString(string(jsoninv))
+	char.AbilitiesJson = template.HTMLEscapeString(string(jsonabi))
+
+	//fmt.Println(char.AttacksJson)
 }
 
 func (att *Attack) Display() string {
@@ -358,6 +406,13 @@ func initChars() {
 		panic(err)
 	}
 
+	for i := range players {
+		char := loadPlayerChar(players[i])
+		char.Playername = players[i]
+		fmt.Println("Loaded ", char.Name)
+		chars = append(chars,char)
+	}
+
 	curhps = make(map[int]int)
 	for i := range chars {
 		curhps[i] = chars[i].HP
@@ -385,7 +440,39 @@ func defaultAssetPath() string {
 	return p.Dir
 }
 
+func alreadyLoggedIn(playername string) bool {
+	for i := range loggedin {
+		if loggedin[i] == playername {
+			return true
+		}
+	}
+
+	return false
+}
+
+func validatePlayer(player string) bool {
+	for i := range players {
+		if strings.ToLower(player) == players[i] {
+			if alreadyLoggedIn(players[i]) {
+				fmt.Println("Already logged in! ", players[i])
+				return false
+			}
+			fmt.Println("Fantastic, you are", player)
+			loggedin = append(loggedin,players[i])
+			return true
+		}
+	}
+
+	return false
+}
+
 func homeHandler(c http.ResponseWriter, req *http.Request) {
+
+	cv,_ := req.Cookie("playername")
+	if cv != nil && cv.Value != "" && !alreadyLoggedIn(cv.Value) {
+			http.SetCookie(c,&http.Cookie{Name: "playername", Value: "", MaxAge: -1})
+	}
+
 
 	if strings.Contains(req.URL.Path, "assets") {
 		//fmt.Println("Serving assets...")
@@ -393,6 +480,24 @@ func homeHandler(c http.ResponseWriter, req *http.Request) {
 
 	} else if strings.Contains(req.URL.Path, "char") {
 		webViewChar(c, req)
+	} else if strings.Contains(req.URL.Path, "playeredit") {
+		fmt.Println("Player edit...")
+		editPlayerChar(c, req)
+	} else if strings.Contains(req.URL.Path, "playerview") {
+		viewPlayerChar(c, req)
+	} else if strings.Contains(req.URL.Path, "playerid") {
+		req.ParseForm()
+		if len(req.Form["playername"]) == 1 && validatePlayer(req.Form["playername"][0]) {
+			http.SetCookie(c,&http.Cookie{Name: "playername", Value: req.Form["playername"][0]})
+			mainData := MainData{Host: req.Host, Content: lastoutput}
+			homeTempl.Execute(c, mainData)
+		} else if cv != nil && cv.Value != "" && alreadyLoggedIn(cv.Value) {
+			mainData := MainData{Host: req.Host, Content: fmt.Sprintf("You are already logged in as %s", cv.Value)}
+			homeTempl.Execute(c, mainData)
+		} else {
+			http.SetCookie(c,&http.Cookie{Name: "playername", Value: "", MaxAge: -1})
+			playerIdTempl.Execute(c,nil)
+		}
 	} else {
 		mainData := MainData{Host: req.Host, Content: lastoutput}
 		homeTempl.Execute(c, mainData)
@@ -457,9 +562,9 @@ func renderParty() string {
 			}
 
 			if ShowMugs {
-				output = output + fmt.Sprintf("<div id=\"%s\" class=\"partymember\"><div><a href=\"/char?name=%s\"><img src=\"/assets/%s\" width=120/></a></div><b>%s</b><br>%s/%s/%s/%d<br>%d/%d   </div>", chars[i].Name, chars[i].Name, chars[i].Image, chars[i].Name, chars[i].Race, chars[i].Class, chars[i].Alignment, chars[i].Level, curhp, chars[i].HP)
+				output = output + fmt.Sprintf("<div id=\"%s\" class=\"partymember\"><div><a href=\"/char?name=%s\"><img src=\"/assets/%s\" width=120/></a></div><b>%s</b><br>%s/%s/%d<br>%d/%d   </div>", chars[i].Name, chars[i].Name, chars[i].Image, chars[i].Name, chars[i].Race, chars[i].Class, chars[i].Level, curhp, chars[i].HP)
 			} else {
-				output = output + fmt.Sprintf("<div id=\"%s\" class=\"partymembernoimg\"><b>%s</b><br>%s/%s/%s/%d<br>%d/%d   </div>", chars[i].Name, chars[i].Name, chars[i].Race, chars[i].Class, chars[i].Alignment, chars[i].Level, curhp, chars[i].HP)
+				output = output + fmt.Sprintf("<div id=\"%s\" class=\"partymembernoimg\"><b>%s</b><br>%s/%s/%d<br>%d/%d   </div>", chars[i].Name, chars[i].Name, chars[i].Race, chars[i].Class, chars[i].Level, curhp, chars[i].HP)
 			}
 		}
 	}
@@ -690,7 +795,8 @@ func viewChar(name string) string {
 		inventory = inventory + obj.Name + "<br>"
 	}
 
-	output = fmt.Sprintf("<div id=\"viewchar\"><img id=\"charimg\" src=\"/assets/%s\"/></div><div id=\"charinfo\"><p>%s, Level %d %s</p><p>%s</p>Str: %d Dex: %d Con: %d Int: %d Wis: %d Cha: %d<br>Initiative: %d<br>HP: %d<br>Alignment: %s<br>Attacks:<br> %s%s</div>", char.Image, char.Name, char.Level, char.Class, char.Desc, char.Abilities.Str, char.Abilities.Dex, char.Abilities.Con, char.Abilities.Int, char.Abilities.Wis, char.Abilities.Cha, char.Initiative, char.HP, char.Alignment, attacks, inventory)
+	output = fmt.Sprintf("<div id=\"viewchar\"><img id=\"charimg\" src=\"/assets/%s\"/></div><div id=\"charinfo\"><p>%s, Level %d %s</p><p>%s</p>Str: %d Dex: %d Con: %d Int: %d Wis: %d Cha: %d<br>Initiative: %d<br>HP: %d<br>Alignment: %s<br>Attacks:<br> %s%s", char.Image, char.Name, char.Level, char.Class, char.Desc, char.Abilities.Str, char.Abilities.Dex, char.Abilities.Con, char.Abilities.Int, char.Abilities.Wis, char.Abilities.Cha, char.Initiative, char.HP, char.Alignment, attacks, inventory)
+	output = output + fmt.Sprintf("<hr>Inspiration: %d<br> Proficiency Bonus: %d<br> Passive Perception: %d<br> Hit Dice: %s<br> Speed: %d<br> Skills: %s<br><hr>Misc Proficiencies and Languages: %s<br> Personality Traits: %s<br> Ideals: %s<br> Bonds: %s<br> Flaws: %s<br> Features and Traits: %s<br> Treasure: %s<br> Spells<br> Level 0: %s<br> Level 1: %s<br> Level 2: %s<br> Level 3: %s<br> Level 4: %s<br> Level 5: %s<br> Level 6: %s<br> Level 7: %s<br> Level 8: %s<br> Level 9: %s<br> </div>", char.Inspiration, char.ProfBonus, char.PassPerception, char.HitDice, char.Speed, char.Skills, char.MiscProfLanguages, char.PersonalityTraits, char.Ideals, char.Bonds, char.Flaws, char.FeaturesTraits, char.Treasure, char.SpellL0, char.SpellL1, char.SpellL2, char.SpellL3, char.SpellL4, char.SpellL5, char.SpellL6, char.SpellL7, char.SpellL8, char.SpellL9)
 
 	//fmt.Println(char.Name)
 	//fmt.Println(output)
@@ -701,13 +807,14 @@ func viewNpcChar(name string) string {
 	output := ""
 	char := getChar(name)
 
-	attacks := "<table><tr><td>name</td>  <td>to hit</td>  <td>dmg</td>  <td>type</td></tr>"
+/*	attacks := "<table><tr><td>name</td>  <td>to hit</td>  <td>dmg</td>  <td>type</td></tr>"
 	for i := range char.Attacks {
 		attacks = attacks + "<tr>\n" + char.Attacks[i].Display() + "</tr>"
 	}
 	attacks = attacks + "</table>"
+	*/
 
-	output = fmt.Sprintf("<div id=\"viewchar\"><img id=\"charimg\" src=\"/assets/%s\"/></div><div id=\"charinfo\"><p>%s</p><p>%s</p>Str: %d Dex: %d Con: %d Int: %d Wis: %d Cha: %d<br>Attacks:<br> %s</div>", char.Image, char.Name, char.Desc, char.Abilities.Str, char.Abilities.Dex, char.Abilities.Con, char.Abilities.Int, char.Abilities.Wis, char.Abilities.Cha, attacks)
+	output = fmt.Sprintf("<div id=\"viewchar\"><img id=\"charimg\" src=\"/assets/%s\"/></div><div id=\"charinfo\"><p>%s</p><p>%s</div>", char.Image, char.Name, char.Desc)
 
 	//fmt.Println(char.Name)
 	//fmt.Println(output)
@@ -931,6 +1038,125 @@ func loopForDMInput() {
 	}
 }
 
+func viewPlayerChar(c http.ResponseWriter, req *http.Request) {
+	if len(req.Form["name"]) == 0 {
+		mainData := MainData{Host: req.Host, Content: lastoutput}
+		homeTempl.Execute(c, mainData)
+	} else {
+		mainData := MainData{Content: viewChar(req.Form["name"][0])}
+		homeTempl.Execute(c, mainData)
+	}
+}
+
+func loadPlayerChar(playername string) Char {
+	filename := fmt.Sprintf("assets/players/%s.json",playername)
+	file, err := os.Open(filename)
+
+	if err != nil {
+		panic(fmt.Sprintf("Could not open %s",filename))
+	}
+
+	filebytes := ReadFileContents(file)
+
+	char := Char{}
+	err = json.Unmarshal(filebytes, &char)
+	if err != nil {
+		fmt.Println("Failed to read player file: ", err)
+		panic(err)
+	}
+
+	return char
+
+}
+
+func updatePlayerChar(req *http.Request) {
+	char := Char{}
+
+	char.Name = req.Form["charname"][0]
+	char.Class = req.Form["class"][0]
+	char.Race = req.Form["race"][0]
+	char.Level,_ = strconv.Atoi(req.Form["level"][0])
+	char.Alignment = req.Form["alignment"][0]
+	abil := Abilities{}
+	json.Unmarshal([]byte(req.Form["abilities"][0]), &abil)
+	char.Abilities = abil
+	//char.Abilities = req.Form["abilities"]
+	char.HP,_ = strconv.Atoi(req.Form["hp"][0])
+	char.AC,_ = strconv.Atoi(req.Form["ac"][0])
+	char.Initiative,_ = strconv.Atoi(req.Form["initiative"][0])
+	//char.Attacks,_ = req.Form["attacks"]
+	atts := make([]Attack,0)
+	json.Unmarshal([]byte(req.Form["attacks"][0]), &atts)
+	char.Attacks = atts
+	//char.Inventory,_ = req.Form["inventory"]
+	inv := make([]string,0)
+	fmt.Println(inv)
+	json.Unmarshal([]byte(req.Form["inventory"][0]), &inv)
+	char.Inventory = inv
+	char.Background = req.Form["background"][0]
+	char.Image = req.Form["image"][0]
+	char.Inspiration,_ = strconv.Atoi(req.Form["inspiration"][0])
+	char.ProfBonus,_ = strconv.Atoi(req.Form["profbonus"][0])
+	char.PassPerception,_ = strconv.Atoi(req.Form["passperception"][0])
+	char.HitDice = req.Form["hitdice"][0]
+	char.Speed,_ = strconv.Atoi(req.Form["speed"][0])
+	char.Skills = req.Form["skills"][0]
+	char.MiscProfLanguages = req.Form["misclang"][0]
+	char.PersonalityTraits = req.Form["personalitytraits"][0]
+	char.Ideals = req.Form["ideals"][0]
+	char.Bonds = req.Form["bonds"][0]
+	char.Flaws = req.Form["flaws"][0]
+	char.FeaturesTraits = req.Form["featurestraits"][0]
+	char.Treasure = req.Form["treasure"][0]
+
+	if req.Form["inparty"][0] == "true" {
+		char.InParty = true
+	} else {
+		char.InParty = false
+	}
+	char.Playername = req.Form["playername"][0]
+
+
+	fmt.Println(char)
+	bytes,_ := json.Marshal(char)
+	fmt.Println(string(bytes))
+
+
+	updatePlayerFile(req.Form["playername"][0],bytes)
+
+}
+
+func updatePlayerFile(charname string, data []byte) {
+
+	filename := fmt.Sprintf("assets/players/%s.json",charname)
+
+	err := ioutil.WriteFile(filename,data,0755)
+
+	if err != nil {
+		panic(fmt.Sprintf("Could not write to %s", filename))
+	}
+}
+
+func editPlayerChar(c http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+
+	playercookie,_ := req.Cookie("playername")
+	fmt.Println("Player cookie: ", playercookie.Value)
+	if len(req.Form["update"]) != 0 {
+		// update player data
+		updatePlayerChar(req)
+		mainData := MainData{Host: req.Host, Content: "Successfully updated the char!<br><a href=\"/\">Return to main view</a><br>"}
+		homeTempl.Execute(c, mainData)
+	} else if playercookie.Value != "" {
+		// send form
+		//mainData := MainData{Content: editPlayerForm(req)}
+		char := loadPlayerChar(strings.ToLower(playercookie.Value))
+		fmt.Println("Loaded ", char.Name)
+		char.JsonIfy()
+		editPlayerTempl.Execute(c, char)
+	}
+}
+
 func webViewChar(c http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 
@@ -946,6 +1172,7 @@ func webViewChar(c http.ResponseWriter, req *http.Request) {
 var chttp = http.NewServeMux()
 
 func initialState() {
+	players = strings.Split(charlist,",")
 	initPlaces()
 	initObjects()
 	initScenes()
@@ -963,13 +1190,18 @@ func initialState() {
 }
 
 func main() {
+	flag.StringVar(&charlist, "chars", "", "Character list separate by commas.")
 	flag.Parse()
+
 	homeTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "home.html")))
+	editPlayerTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "editplayer.html")))
+	playerIdTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "playerid.html")))
 
 	initialState()
 
 	lastoutput = renderContent("", &Command{})
 
+	loggedin = make([]string,0)
 	go h.run()
 	go loopForDMInput()
 
